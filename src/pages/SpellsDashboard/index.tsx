4af0afe3fd079cf013e86d9a676309e92bbed239
Matheus Alexandria <maalexandria@hotmail.com>
2020-11-09 21:43:52 -0300

    Chore: Dashboard HTML and CSS updated

--- src/pages/SpellsDashboard/index.tsx
+++ src/pages/SpellsDashboard/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
 import ImgPotion from '../../assets/potion2.png';
+import ImgBackground from '../../assets/shadowfell.png';
+import ImgBook from '../../assets/book.png';
 
-import { Container, Header, HeaderContent, HeaderOptions } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  HeaderOptions,
+  Navigator,
+  Dashboard,
+  IntroductionChart,
+  OptionsBook,
+} from './styles';
 
 const SpellsDashboard: React.FC = () => {
   return (
@@ -20,6 +31,36 @@ const SpellsDashboard: React.FC = () => {
           <button type="button">bla bla bla</button>
         </HeaderOptions>
       </Header>
+
+      <Navigator>
+        <button type="button">PI PI PI</button>
+        <button type="button">PO PO PO</button>
+        <button type="button">PI PI PI</button>
+        <button type="button">PO PO PO</button>
+      </Navigator>
+
+      <Dashboard>
+        <img src={ImgBackground} alt="Background" />
+
+        <IntroductionChart>
+          <strong>TÍTULO</strong>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            vitae tortor sem. Nulla facilisi. Fusce sit amet imperdiet tellus,
+            at tempor diam. Quisque leo dui, faucibus eget ipsum in, sagittis
+            tempus risus. Praesent eleifend nisi at nisi ornare, sed dignissim
+            enim cursus. Sed ligula mi, dignissim nec magna id, euismod
+            malesuada justo. Aenean non imperdiet diam, eu tincidunt purus.
+            Pellentesque vel cursus ante. Duis rhoncus, urna quis consectetur
+            iaculis, nulla elit condimentum nunc, pretium varius tortor dolor ut
+            neque.
+          </p>
+        </IntroductionChart>
+      </Dashboard>
+
+      <OptionsBook>
+        <img src={ImgBook} alt="Um livro como imagem de um menu" />
+      </OptionsBook>
     </Container>
   );
 };
--- src/pages/SpellsDashboard/styles.ts
+++ src/pages/SpellsDashboard/styles.ts
@@ -17,8 +17,8 @@ export const HeaderContent = styled.div`
   justify-content: flex-start;
 
   img {
-    width: 98px;
-    height: 98px;
+    width: 88px;
+    height: 88px;
     margin-left: 30px;
   }
 
@@ -26,7 +26,7 @@ export const HeaderContent = styled.div`
     color: #d6975e;
     font-family: 'Mina', sans-serif;
     font-weight: bold;
-    font-size: 70px;
+    font-size: 60px;
   }
 `;
 
@@ -48,5 +48,84 @@ export const HeaderOptions = styled.div`
     border: none;
     font-family: 'Mina', sans-serif;
     font-size: 26px;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+export const Navigator = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #383232;
+  padding: 8px 160px;
+
+  button {
+    background-color: transparent;
+    color: #d6972d;
+    border: none;
+    font-family: 'Mina', sans-serif;
+    font-size: 18px;
+    transition: 0.2s;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+export const Dashboard = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  img {
+    width: 100%;
+  }
+`;
+
+export const IntroductionChart = styled.div`
+  position: absolute;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 480px;
+  height: 580px;
+  background-color: #383232;
+  padding: 40px;
+  background: rgba(0, 0, 0, 0.6);
+  margin-left: 90px;
+
+  strong {
+    opacity: 1;
+    color: #d6972d;
+    margin-bottom: 20px;
+    font-family: 'Mina', sans-serif;
+    font-size: 22px;
+  }
+
+  p {
+    opacity: 1;
+    color: #d6972d;
+    line-height: 32px;
+    font-family: 'Mina', sans-serif;
+    font-size: 16px;
+  }
+`;
+
+export const OptionsBook = styled.div`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  margin-right: 20px;
+  margin-bottom: 10px;
+
+  img {
+    width: 100px;
   }
 `;
